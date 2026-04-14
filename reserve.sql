c7995ca58591df1fee1cb35201bb7a2ead82b8b5
unknown <dkswnsgus66@gmail.com>
2026-04-14 17:39:07 +0900

    db정리

--- reserve.sql
+++ reserve.sql
@@ -1,15 +1,17 @@
--- create database reserve;
--- use reserve;
+create database reserve;
+use reserve;
+
 -- 상영관
 create table room(
-                     id bigint primary key auto_increment,
-                     room_number int not null
+                     id int primary key auto_increment,
+                     movie_id bigint not null ,
+                     is_available boolean default true,
+                     foreign key (movie_id) references movies(id)
 );
-
 -- 좌석
 create table seat(
                      id bigint primary key auto_increment,
-                     room_id bigint not null,
+                     room_id int not null,
                      seat_number int not null,
                      is_available boolean default true,
 
@@ -24,13 +26,15 @@ create table movies(
                        title varchar(100) not null,
                        grade varchar(10) default '일반',
                        price decimal(10,2) not null,
-                       view_count int default 0
+                       view_count int default 0,
+                       is_available boolean default true
 );
 
 -- 고객
 create table customer(
                          id bigint primary key auto_increment,
                          email varchar(100) not null unique,
+                         password varchar(50) not null,
                          name varchar(50) not null,
                          age int not null,
                          create_at datetime default current_timestamp,
@@ -38,15 +42,18 @@ create table customer(
 );
 
 -- 예약
+-- 예약
 create table reservation(
                             id bigint primary key auto_increment,
                             customer_id bigint not null,
                             movie_id bigint not null,
-                            room_id bigint not null,
+                            room_id int not null,
                             seat_id bigint not null,
-
+                            is_available boolean default false,
                             foreign key (customer_id) references customer(id),
                             foreign key (movie_id) references movies(id),
                             foreign key (room_id) references room(id),
                             foreign key (seat_id) references seat(id)
 );
+
+drop database reserve;
