fc9ea99e934a9308cd426e4ccbe80f95ad6664b8
김다영 <darong9494@gmail.com>
2026-04-14 17:07:41 +0900

    movieDAO

--- src/main/java/com/tenco/dao/RoomDAO.java
+++ src/main/java/com/tenco/dao/RoomDAO.java
@@ -1,4 +1,48 @@
 package com.tenco.dao;
 
 public class RoomDAO {
+<<<<<<< Updated upstream
+=======
+    // 현재 상영관에서 상영하는 영화 정보
+    public List<Room> findAll() {
+        List<Room> roomList = new ArrayList<>();
+        String sql = """
+                    SELECT * FROM room;
+                """;
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                roomList.add(mapToRoom(rs));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return roomList;
+    }
+
+    // 이용(개방) 가능 여부 변경
+    public Boolean useStatus(){
+
+
+        return false;
+    }
+
+    // todo 지워야됨
+    public static void main(String[] args) {
+        RoomDAO dao = new RoomDAO();
+        List<Room> roomList = dao.findAll();
+
+        System.out.println(roomList);
+    }
+
+    private Room mapToRoom(ResultSet rs) throws SQLException {
+        return Room.builder()
+                .id(rs.getInt("id"))
+                .roomNumber(rs.getInt("seat"))
+                .isAvailable(rs.getBoolean(("is_Available")))
+                .build();
+    }
+>>>>>>> Stashed changes
 }
